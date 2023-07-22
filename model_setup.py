971badda14b59a150457639a696062346f181e7f
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-07-22 13:14:31 +0300

    returning bool

--- model_setup.py
+++ model_setup.py
@@ -4,16 +4,24 @@ import numpy as np
 
 model = tf.keras.models.load_model('models/tyre_condition.h5')
 
+def model_predict(img_filepath):
 
-test_image = image.load_img(r"C:\Users\sas\Downloads\tyre3_img.png", target_size = (224, 224)) # Get the image
+    test_image = image.load_img(img_filepath, target_size = (224, 224)) # Get the image
 
-transformed_image = image.img_to_array(test_image) # Transform image to array
-transformed_image = np.expand_dims(transformed_image, axis=0) # Expand dims to make a batch of 1 image
-prediction = model.predict(transformed_image)
+    transformed_image = image.img_to_array(test_image) # Transform image to array
+    transformed_image = np.expand_dims(transformed_image, axis=0) # Expand dims to make a batch of 1 image
+    prediction = model.predict(transformed_image)
 
-print(prediction)
+    print(prediction)
 
-if prediction[0] < 0.5:
-  print("This tyre HAS a problem!")
-else:
-  print("This tyre HASN'T problem!")
+    result = None
+
+    if prediction[0] < 0.5:
+        result = True
+    else:
+        result = False
+    
+    return result
+
+model_result = model_predict(r"C:\Users\sas\Downloads\tyre3_img.png")
+print(model_result)
